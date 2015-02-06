e50e67724ec0aaf842cb4eed05cdcb2e7c9c8269
Nick Fredmangit config --global ntfredman@gmail.comgit config --global user.name Nick <ntfredman@gmail.com>
2015-02-06 15:26:55 -0700

    Done with what I want to do

--- main.js
+++ main.js
@@ -16,56 +16,69 @@ $(document).on('ready', function() {
 
 // Functionality to add a quote
 
-  $('.add-quote-button').on('click', function(e) {
-    var tempQuote = $('.quote-text').val();
-    var tempAuthor = $('.quote-author').val();
-    quoteList.push({"quote": tempQuote, "author": tempAuthor, "stars": 0});
-    var pQuote = $('<p class="quote-list-text">"' + tempQuote + '"</p>');
-    var pAuthor = $('<p class="quote-list-author">- ' + tempAuthor + '</p>');
-    var closeButton = $('<button class="rem-quote"> x </button>');
+    $('.add-quote-button').on('click', function(e) {
+        var tempQuote = $('.quote-text').val();
+        var tempAuthor = $('.quote-author').val();
+        var willBeAQuote = {"quote": tempQuote, "author": tempAuthor, "stars": 0};        quoteList.push(willBeAQuote);
+        var pQuote = $('<p class="quote-list-text">"' + tempQuote + '"</p>');
+        var pAuthor = $('<p class="quote-list-author">- ' + tempAuthor + '</p>');
+        var closeButton = $('<button class="rem-quote"> x </button>');
 
-    var starLi1 = ('<li class="star" id="1"></li>');
-    var starLi2 = ('<li class="star" id="2"></li>');
-    var starLi3 = ('<li class="star" id="3"></li>');
-    var starLi4 = ('<li class="star" id="4"></li>');
-    var starLi5 = ('<li class="star" id="5"></li>');
+        var star = ('<li class="star"></li>');
 
-    var starDiv = $('<div class="stars"><ul>' + starLi1 + starLi2 + starLi3 + starLi4 + starLi5 + '</ul></div>');
+        var starDiv = $('<div class="stars"><ul>' + star + star + star + star + star + '</ul></div>');
 
-    var thisQuote = $('<div class="live-quote">')
-        .append(pQuote)
-        .append(pAuthor)
-        .append(closeButton)
-        .append(starDiv)
-        .append('</div>');
+        var thisQuote = $('<div class="live-quote">')
+            .append(pQuote)
+            .append(pAuthor)
+            .append(closeButton)
+            .append(starDiv)
+            .data('item', willBeAQuote);
 
-    $('.quote-list')
-        .append(thisQuote)
 
-    e.preventDefault();
+        $('.quote-list')
+            .append(thisQuote);
 
-    console.log(quoteList);
-  });
+        e.preventDefault();
 
-  $(document).on('click', '.rem-quote', function() {
-    $(this).parent().remove();
-  });
+        console.log(quoteList);
+    });
+
+    $(document).on('click', '.rem-quote', function() {
+        $(this).parent().remove();
+    });
 
 
 //star function needs to know how many stars in were clicked, can you check stars from top or bottom?
 //Then it needs to update the total list based on the number of stars
 //maybe this can be done with an array of stars...per div? I dunno this is super tough now, not sure how to sort them by this....
 //Added items to the object to do this
-  $(document).on('click', '.star', function() {
-    if($(this).attr("id", "1") {
-    }
+//
 
 
+    $(document).on('click', '.star', function() {
+        var index = $(this).index();
+        $(this).parent().children().removeClass('clicked-star');
+        $(this).parent().children(":lt(" + index + ")").addClass('clicked-star');
+        $(this).addClass('clicked-star');
+        var actualQuote = $(this).closest('.live-quote').data('item');
+        actualQuote.stars = index;
 
+    });
 
+    $(document).on('mouseover', '.star', function() {
+        var index = $(this).index();
+        $(this).parent().children().removeClass('clicked-star');
+        $(this).parent().children(":lt(" + index + ")").addClass('clicked-star');
+        $(this).addClass('clicked-star');
+        console.log(index);
+        console.log($(this).parent());
+    }).on('mouseout', '.star', function(){
+            var actualQuote = $(this).closest('.live-quote').data('item');
+            $(this).parent().children().removeClass('clicked-star');
+            $(this).parent().children(":lt(" + actualQuote.stars + ")").addClass('clicked-star');
+            $(this).addClass('clicked-star');
+    };
 
-
-
-  });
 
 });
